c2b9bd8a79cbd280414434a9896215852b2ff561
rderoncio <rafael.deroncio@hotmail.com>
2023-02-27 21:27:30 -0300

    Configure service e pipeline ImageSharp

--- App/Program.cs
+++ App/Program.cs
@@ -1,10 +1,27 @@
 // Services
+using SixLabors.ImageSharp.Web.Caching;
+using SixLabors.ImageSharp.Web.DependencyInjection;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
+builder.Services.AddImageSharp(
+    options =>
+    {
+        options.BrowserMaxAge = TimeSpan.FromDays(7); // Duração do cache de imagem no nagevador
+        options.CacheMaxAge = TimeSpan.FromDays(365.25); // Duração do cache de imagem no servidor
+        options.CacheHashLength = 8; // Nome do arquivos em cache com no máximo 8 caracteres 
+    }
+    ).Configure<PhysicalFileSystemCacheOptions>(
+    options =>
+    {
+        options.CacheFolder = "img/cache"; // default: wwwroot/ 
+    });
 
 // Configure
 WebApplication app = builder.Build();
 app.UseDeveloperExceptionPage();
+app.UseImageSharp();
 app.UseStaticFiles();
 app.UseRouting();
 
