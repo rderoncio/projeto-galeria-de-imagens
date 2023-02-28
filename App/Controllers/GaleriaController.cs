bece78106a82a8837cfd22e8924de01b38c3c180
rderoncio <rafael.deroncio@hotmail.com>
2023-02-28 13:47:08 -0300

    ajustes e add view  index de galeria

--- App/Controllers/GaleriaController.cs
+++ App/Controllers/GaleriaController.cs
@@ -1,11 +1,22 @@
+using App.Context;
+using App.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace App.Controllers;
 
 public class GaleriaController : Controller
 {
+    private readonly DatabaseContext _database;
+    private readonly IProcessadorImagem _processarImagemService;
+
+    public GaleriaController(DatabaseContext database, IProcessadorImagem processarImagemService)
+    {
+        _database = database;
+        _processarImagemService = processarImagemService;
+    }
     public IActionResult Index()
     {
-        return View();
+        return View(_database.Galeria.AsNoTracking().ToList());
     }
 }
