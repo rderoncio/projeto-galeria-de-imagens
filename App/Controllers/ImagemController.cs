e719201a2897f8442b1af437889ce7066f0f56da
rderoncio <rafael.deroncio@hotmail.com>
2023-03-01 10:51:39 -0300

    Add aplicar efeito

--- App/Controllers/ImagemController.cs
+++ App/Controllers/ImagemController.cs
@@ -1,4 +1,5 @@
 using App.Context;
+using App.Enums;
 using App.Models;
 using App.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
@@ -48,7 +49,7 @@ public class ImagemController : Controller
             return NotFound();
 
         ImagemModel imagem = new() { IdGaleria = galeria.IdGaleria };
-        
+
         return View(imagem);
     }
 
@@ -60,7 +61,7 @@ public class ImagemController : Controller
 
         _database.Add(imagem);
 
-        if(_database.SaveChanges() > 0)
+        if (_database.SaveChanges() > 0)
         {
             string caminhoArquivoImagem = ObterCaminhoImagem("\\img\\", imagem.IdImagem, "webp");
             _processarImagemService.SalvarUploadImagemAsync(caminhoArquivoImagem, imagem.ArquivoImagem).Wait();
@@ -79,7 +80,7 @@ public class ImagemController : Controller
 
         if (imagem is null)
             return NotFound();
-        
+
         return View(imagem);
     }
 
@@ -96,9 +97,9 @@ public class ImagemController : Controller
         if (!(_database.SaveChanges() > 0))
             return NotFound();
 
-            string caminhoArquivoImagem = ObterCaminhoImagem("\\imagens\\", imagem.IdImagem, "webp");
-            _processarImagemService.SalvarUploadImagemAsync(caminhoArquivoImagem, imagem.ArquivoImagem).Wait();
-        
+        string caminhoArquivoImagem = ObterCaminhoImagem("\\img\\", imagem.IdImagem, "webp");
+        _processarImagemService.SalvarUploadImagemAsync(caminhoArquivoImagem, imagem.ArquivoImagem).Wait();
+
         return RedirectToAction(nameof(Index), new { id = imagem.IdGaleria });
     }
 
@@ -112,7 +113,7 @@ public class ImagemController : Controller
 
         if (imagem is null)
             return NotFound();
-        
+
         return View(imagem);
     }
 
@@ -120,23 +121,85 @@ public class ImagemController : Controller
     [ActionName("Excluir")]
     public IActionResult ExcluirPost(int? id)
     {
-        if(!id.HasValue)
+        if (!id.HasValue)
             return NotFound();
 
         ImagemModel imagem = _database.Imagens.Find(id.Value);
 
-        if(imagem is null)
+        if (imagem is null)
             return NotFound();
 
         _database.Remove(imagem);
 
-        if(_database.SaveChanges() > 0)
+        if (_database.SaveChanges() > 0)
         {
-            string caminhoArquivoImagem = ObterCaminhoImagem("\\imagens\\", imagem.IdImagem, "webp");
+            string caminhoArquivoImagem = ObterCaminhoImagem("\\img\\", imagem.IdImagem, "webp");
             _processarImagemService.ExcluirImagemAsync(caminhoArquivoImagem).Wait();
         }
 
         return RedirectToAction(nameof(Index), "Imagem", new { id = imagem.IdGaleria });
+    }
+
+    [HttpGet]
+    public IActionResult AplicarEfeito(int? id)
+    {
+        if (id is null)
+            return NotFound();
+
+        ImagemModel imagem = _database.Imagens.Find(id.Value);
+
+        if (imagem is null)
+            return NotFound();
+
+        return View(nameof(Efeitos), imagem);
+    }
+
+    [HttpPost]
+    public IActionResult AplicarEfeito(int id, string efeito)
+    {
+        string caminhoArquivoImagem = ObterCaminhoImagem("\\img\\", id, "webp");
+
+        switch (efeito)
+        {
+            case "rdireita":
+                _processarImagemService.AplicarEfeitoImagemAsync(caminhoArquivoImagem, EfeitoImagem.RotacionarDireita).Wait();
+                break;
+
+            case "resquerda":
+                _processarImagemService.AplicarEfeitoImagemAsync(caminhoArquivoImagem, EfeitoImagem.RotacionarEsquerda).Wait();
+                break;
+
+            case "ihorizontal":
+                _processarImagemService.AplicarEfeitoImagemAsync(caminhoArquivoImagem, EfeitoImagem.InverterHorizontal).Wait();
+                break;
+
+            case "ivertical":
+                _processarImagemService.AplicarEfeitoImagemAsync(caminhoArquivoImagem, EfeitoImagem.InverterVertical).Wait();
+                break;
+                
+            case "ecinza":
+                _processarImagemService.AplicarEfeitoImagemAsync(caminhoArquivoImagem, EfeitoImagem.EscalaDeCinza).Wait();
+                break;
+
+            case "sepia":
+                _processarImagemService.AplicarEfeitoImagemAsync(caminhoArquivoImagem, EfeitoImagem.Sepia).Wait();
+                break;
+
+            case "negativo":
+                _processarImagemService.AplicarEfeitoImagemAsync(caminhoArquivoImagem, EfeitoImagem.Negativo).Wait();
+                break;
+
+            case "desfoque":
+                _processarImagemService.AplicarEfeitoImagemAsync(caminhoArquivoImagem, EfeitoImagem.Desfoque).Wait();
+                break;
+        }
+
+        return RedirectToAction(nameof(AplicarEfeito), new { id = id });
+    }
+
+    public IActionResult Efeitos()
+    {
+        return View();
     }
 
     #region Metodos privados
